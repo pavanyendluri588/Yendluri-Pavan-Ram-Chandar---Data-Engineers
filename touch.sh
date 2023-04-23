e9a5da68cc769488f09ae19662f3f21f912cf35b
pavanyendluri588 <pavanyendluri588@gmail.com>
2023-04-23 22:04:49 +0530

    Consistent Learner

---
+++ touch.sh
@@ -0,0 +1,9 @@
+git init
+git remote add origin https://github.com/pavanyendluri588/Yendluri-Pavan-Ram-Chandar-Data-Engineers.git
+git remote -v  --force
+git add . --force
+git commit -m "Consistent Learner"
+
+
+git push -f origin master
+
